d6d2acea71c6f583f1a7b6afea2d37d4f3397521
Евгения Ковтун <eekovtun@gmail.com>
2021-08-31 15:17:55 +0300

    QRC-14 removed liquibase grants

--- .liquibase/1.0.0/grant/01_auth.sql
+++
@@ -1,8 +0,0 @@
---liquibase formatted sql
-
---changeset eekovtun:1.0.0/grants/auth context:!local
---rollback revoke all on schema auth from auth_handler;
-grant connect on database qrcheck to auth_handler;
-grant usage on schema auth to auth_handler;
-grant select, insert, update, delete on all tables in schema auth to auth_handler;
-grant usage, select on all sequences in schema auth to auth_handler;
