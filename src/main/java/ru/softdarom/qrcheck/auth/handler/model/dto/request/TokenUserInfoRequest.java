1839cfec2c016ea4f9a2333e58ff26d4622daab5
Evgeniya Kovtun <eekovtun@gmail.com>
2021-11-02 16:34:28 +0300

    QRC-47 (#15)
    added user picture

--- src/main/java/ru/softdarom/qrcheck/auth/handler/model/dto/request/TokenUserInfoRequest.java
+++ src/main/java/ru/softdarom/qrcheck/auth/handler/model/dto/request/TokenUserInfoRequest.java
@@ -7,13 +7,17 @@ import ru.softdarom.qrcheck.auth.handler.model.dto.ProviderTokenDto;
 import ru.softdarom.qrcheck.auth.handler.model.dto.ProviderUserDto;
 import ru.softdarom.qrcheck.auth.handler.util.JsonHelper;
 
+import javax.validation.constraints.NotNull;
+
 @Generated
 @Data
 public class TokenUserInfoRequest {
 
+    @NotNull
     @JsonProperty("user")
     private final ProviderUserDto user;
 
+    @NotNull
     @JsonProperty("token")
     private final ProviderTokenDto token;
 
