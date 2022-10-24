f3b6bb586102d81c38c0565963c172093888d641
Evgeniya Kovtun <eekovtun@gmail.com>
2022-10-24 13:04:21 +0300

    QRC-180 (#49)

--- src/main/resources/liquibase/1.0.0/dml/02_microservices.sql
+++ src/main/resources/liquibase/1.0.0/dml/02_microservices.sql
@@ -1,5 +1,5 @@
 --liquibase formatted sql
 
---changeset eekovtun:1.0.0/dml/microservices
+--changeset eekovtun:1.0.0/dml/microservices context:local
 --rollback delete from auth.microservices where id != null;
 insert into auth.microservices (name) values ('auth-handler');
